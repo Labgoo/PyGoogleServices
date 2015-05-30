1df3f92f9fe8c676e93ff5cdaa1f230f533bc166
romankor <kournjaev@gmail.com>
2015-05-29 17:26:42 -0700

    fix utc now bug

--- googleservices/bigquery/utils.py
+++ googleservices/bigquery/utils.py
@@ -8,5 +8,5 @@ def get_table_id(table_id, d):
 
 
 def get_daily_table_names(name_pattern, days_ahead):
-    today = datetime.datetime.utcnow()
+    today = datetime.utcnow()
     return [get_table_id(name_pattern, today + timedelta(days=d)) for d in xrange(days_ahead)]
