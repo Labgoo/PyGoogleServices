b242cec7eec6c15f6df4b3e005183cc1f157b181
Neta Krakover <neta@wondermall.com>
2016-04-01 10:23:31 -0700

    reverted attempt to use only setup.

--- setup.py
+++ setup.py
@@ -51,8 +51,6 @@ setup(
     keywords=[],
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     setup_requires=[
-        'google-api-python-client==1.4.2',
-        'oauth2client==1.5.2',
         'setuptools==20.6.7',
         'setuptools-lint==0.4.1',
         'unittest2',
