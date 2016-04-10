301f09b583dfc3d781d865a2fb8db05373dd2397
Neta Krakover <neta@wondermall.com>
2016-04-10 11:58:20 +0300

    Fixed pylint errors and merge problems.

--- googleservices/cloudmonitoring/client.py
+++ googleservices/cloudmonitoring/client.py
@@ -35,34 +35,70 @@ class GoogleCloudMonitoringClient(GoogleCloudClient):
         machine_zone = get_gce_zone(http)
         machine_zone = machine_zone[machine_zone.rfind('/')+1:]
 
-        timeseries_dict = { 'timeSeries': [{
-              'metric': {
-                  'type': 'custom.googleapis.com/custom/%s' % metric_name,
-                  'labels': {},
-              },
-              'resource': {
-                  'type': 'gce_instance',
-                  'labels': {
-                      'instance_id': unique_machine_id,
-                      'zone': machine_zone
-                   }
-              },
-              'metricKind': 'GAUGE',
-              'valueType': 'double',
-              'points': [ {
-                    'interval': {
-                        'endTime': timestamp,
-                        'startTime': timestamp,
-                        },
-                    'value': {
-                        'doubleValue': int_value,
-                     },
-                 }],
-            }],
+        timeseries_dict = {'timeSeries': [{
+            'metric': {
+                'type': 'custom.googleapis.com/custom/%s' % metric_name,
+                'labels': {},
+            },
+            'resource': {
+                'type': 'gce_instance',
+                'labels': {
+                    'instance_id': unique_machine_id,
+                    'zone': machine_zone
+                }
+            },
+            'metricKind': 'GAUGE',
+            'valueType': 'double',
+            'points': [{
+                'interval': {
+                    'endTime': timestamp,
+                    'startTime': timestamp,
+                },
+                'value': {
+                    'doubleValue': int_value,
+                },
+            }]}],
         }
 
         resp, _ = http.request(
             uri='https://monitoring.googleapis.com/v3/projects/%s/timeSeries' % project_id,
+            method='POST',
+            headers={'Content-Type': 'application/json'},
+            body=json.dumps(timeseries_dict))
+
+        if int(resp['status']) != 200:
+            logging.error('Error reporting to cloud monitoring: %s from instance %s', resp, unique_machine_id)
+            raise CloudMonitoringError('received error %s while attempting to write time series data' % resp['status'])
+
+    def write_timeseries_custon_instance_metric_int_value_v2(self, project_id, metric_name, int_value):
+        # pylint: disable=C0330
+        timestamp = get_timestamp_RFC3375()
+        http = self.get_http_for_request()
+
+        # Writing not supported by API - TODO REST call remove once api supports this
+        unique_machine_id = get_gce_unique_id(http)
+
+        timeseries_dict = {
+            'kind': 'cloudmonitoring#writeTimeseriesRequest',
+            'commonLabels': {
+                'compute.googleapis.com/resource_type': 'instance',
+                'compute.googleapis.com/resource_id': unique_machine_id
+            },
+            'timeseries': [{
+                'timeseriesDesc': {
+                    'metric': 'custom.cloudmonitoring.googleapis.com/custom/%s' % metric_name,
+                    'labels': {}
+                },
+                'point': {
+                    'start': timestamp,
+                    'end': timestamp,
+                    'doubleValue': int_value
+                }
+            }]
+        }
+
+        resp, _ = http.request(
+            uri='https://www.googleapis.com/cloudmonitoring/v2beta2/projects/%s/timeseries:write' % project_id,
             method='POST',
             headers={'Content-Type': 'application/json'},
             body=json.dumps(timeseries_dict))
@@ -84,60 +120,6 @@ class GoogleCloudMonitoringClient(GoogleCloudClient):
         self.credentials.refresh(_http)
 
         return _http
-
-    def write_timeseries_custon_instance_metric_int_value_v2(self, project_id, metric_name, int_value):
-        timestamp = get_timestamp_RFC3375()
-        http = self.get_http_for_request()
-
-        # Writing not supported by API - TODO REST call remove once api supports this
-        unique_machine_id = get_gce_unique_id(http)
-        machine_zone = get_gce_zone(http)
-        machine_zone = machine_zone[machine_zone.rfind('/')+1:]
-
-        timeseries_dict = {
-            'timeSeries': [{
-                'metric': {
-                    'type': 'custom.googleapis.com/custom/%s' % metric_name,
-                    'labels': {},
-                },
-                'resource': {
-                    'type': 'gce_instance',
-                    'labels': {
-                        'instance_id': unique_machine_id,
-                        'zone': machine_zone
-                    }
-                },
-                'metricKind': 'GAUGE',
-                'valueType': 'INT64',
-                'points': [{
-                    'interval': {
-                        'endTime': timestamp,
-                        'startTime': timestamp,
-                    },
-                    'value': {
-                        'doubleValue': double_value,
-                    },
-                }],
-            }],
-        }
-
-        resp, _ = http.request(
-            uri='https://monitoring.googleapis.com/v3/projects/%s/timeSeries' % project_id,
-            method='POST',
-            headers={'Content-Type': 'application/json'},
-            body=json.dumps(timeseries_dict))
-
-        if int(resp['status']) != 200:
-            logging.error('Error reporting to cloud monitoring: %s from instance %s', resp, unique_machine_id)
-            raise CloudMonitoringError('received error %s while attempting to write time series data' % resp['status'])
-
-    @property
-    def credentials(self):
-        if not self._credentials:
-            self._credentials = get_google_credentials(self.use_jwt_credentials_auth, self.jwt_account_name,
-                                                       self.jwt_key_func, self.oauth_credentails_file)
-        return self._credentials
-
 
     @property
     def api_client(self):
